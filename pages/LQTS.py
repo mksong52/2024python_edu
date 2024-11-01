fa044450ad541de9efe787d7d43945f9b459ee16
mksong52 <mksong00@snu.ac.kr>
2024-11-01 20:37:53 +0900

    Update LQTS.py

--- pages/LQTS.py
+++ pages/LQTS.py
@@ -1,1 +1,12 @@
-#
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Plotting a boxplot of Dx_QTc grouped by Large_genogroup
+plt.figure(figsize=(12, 8))
+sns.boxplot(data=df, x='Large_genogroup', y='Dx_QTc')
+plt.title('Distribution of Dx_QTc by Large Genogroup')
+plt.xlabel('Large Genogroup')
+plt.ylabel('Dx_QTc')
+plt.xticks(rotation=45)
+plt.show()
+
